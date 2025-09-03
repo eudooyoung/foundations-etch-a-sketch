74f14fe88af8b723a68445e2b0c8cdecf772e63f
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2025-09-03 13:52:52 +0900

    Complete the proto type

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 const container = document.querySelector(".container");
-
 const rows = [];
-for (let i = 0; i < 4; i++) {
-  rows[i] = document.createElement("div");
-  rows[i].classList.add("row");
-  container.appendChild(rows[i]);
 
-  const cols = [];
-  for (let j = 0; j < 4; j++) {
-    cols[j] = document.createElement("div");
-    cols[j].classList.add("col");
-    rows[i].appendChild(cols[j]);
+function createGrid(side) {
+  const basis = 850 - side;
+
+  for (let i = 0; i < side; i++) {
+    rows[i] = document.createElement("div");
+    rows[i].classList.add("row");
+    rows[i].style.flexBasis = basis + "px";
+    container.appendChild(rows[i]);
+
+    const cols = [];
+    for (let j = 0; j < side; j++) {
+      cols[j] = document.createElement("div");
+      cols[j].classList.add("col");
+      cols[j].style.flexBasis = basis + "px";
+      rows[i].appendChild(cols[j]);
+    }
   }
+
+  const squares = document.querySelectorAll(".col");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.background = "#ee0000ff";
+    });
+  });
 }
 
-const squares = document.querySelectorAll(".col");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", (e) => {
-    e.target.style.background = "#ee0000ff";
-  });
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const side = prompt("Enter the number of squares per a line you want.");
+  rows.forEach((row) => row.remove());
+  createGrid(side);
 });
+
+createGrid(16);
